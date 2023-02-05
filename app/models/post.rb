74ac1be833512267e8c0724d48ffbceb9277c2da
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-05 07:36:47 +0000

    [add]association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+  has_one_attached :post_image
+  has_many :favorites
+  has_many :comments
+  has_many :tag_relations
+  belongs_to :user
+
+
+  def get_post_image(width, height)
+    unless post_image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      post_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    end
+      post_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
--- app/models/tag_relation.rb
+++ app/models/tag_relation.rb
@@ -1,2 +1,5 @@
 class TagRelation < ApplicationRecord
+
+  belongs_to :post
+  belongs_to :tag
 end
