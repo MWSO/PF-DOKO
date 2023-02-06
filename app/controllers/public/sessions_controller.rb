5ceb78d70b790622cd91ecb24d7693188c0dae90
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-06 06:57:31 +0000

    [add]user_CRUD

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  #退会済みのユーザーログインを弾く
+   before_action :user_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +20,16 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  #ステータスチェック(論理削除用)
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+     redirect_to new_user_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,17 +1,33 @@
 class Public::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
   end
 
   def my_page
+    @user = current_user
   end
 
   def withdrawal
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :like, :profile_image)
   end
 
 end
