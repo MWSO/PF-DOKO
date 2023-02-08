7b1e7af927ad7bb2fd878c60492b49d91adf3dba
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-08 08:16:10 +0000

    [add]admin_users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,23 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.all
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to admin_users_path
+  end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:is_deleted)
+  end
+
 end
