5a1936e39934e6c2d1126bb75e78f1b799caa384
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-10 08:20:19 +0000

    [end]comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,6 +1,10 @@
 class Public::CommentsController < ApplicationController
 
-  def index
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments.all
+    @comment_new = Comment.new
+
   end
 
   def create
@@ -8,22 +12,19 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
     @comment.save
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-    @comment.update(comment_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
+    redirect_to post_path(@comment.post_id)
   end
 
  private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :user_id, :post_id)
   end
 
 end
