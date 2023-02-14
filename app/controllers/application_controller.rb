9c488b6abb7712c49585428c9a155b0c74b6eadd
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-14 08:06:30 +0000

    [add]layout_home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,24 @@ class ApplicationController < ActionController::Base
     @search_result = @search_posts.result
   end
 
+  private
+
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(User)
+        my_page_path
+      else
+        root_path
+      end
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :user
+        root_path
+      elsif resource_or_scope == :admin
+        new_admin_session_path
+      else
+        root_path
+      end
+    end
 
 end
