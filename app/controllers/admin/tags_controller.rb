dfe80a72186a6a40cb60232317b5e875a4cdd19d
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-18 13:06:38 +0000

    [layout]admin_all

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -2,7 +2,7 @@ class Admin::TagsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @tags = Tag.all
+    @tags = Tag.all.page(params[:page]).per(50)
   end
 
   def destroy
