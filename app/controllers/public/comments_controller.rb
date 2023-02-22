614195434684abe1389d812299641045f3829745
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-02-22 10:23:02 +0000

    [nochange]

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,7 +3,7 @@ class Public::CommentsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.all.page(params[:page]).per(20)
+    @comments = @post.comments.all.order(id: "DESC").page(params[:page]).per(20)
     @comment_new = Comment.new
 
   end
