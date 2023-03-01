2da6986a0b06faf587fa3c6e27d4350e4303bde8
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-03-01 13:27:16 +0000

    [fixed]admin_comment_link

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -5,4 +5,10 @@ class Admin::CommentsController < ApplicationController
     @comments = Comment.all.page(params[:page]).per(20)
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_user_path(comment.user_id)
+  end
+
 end
